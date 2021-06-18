7f36d6a2d4576813df5f53802118db2df3507c1a
titoanugerah <titoanugerah@student.undip.ac.id>
2021-06-18 23:35:35 +0700

    Added modification

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Payroll.Models
 {
@@ -10,6 +12,15 @@ namespace Payroll.Models
         public int NIK { set; get; }
         [Required]
         public string Name { set; get; }
+        [MaxLength(1)]
+        [DefaultValue("L")]
+        public char Sex { set; get; }
+        [Required]
+        public string BirthPlace { set; get; }
+        [Required]
+        public DateTime BirthDate { set; get; }
+        [Required]
+        public string Religion { set; get; }
         [Required]
         public string Address { set; get; }
         [Required]
@@ -20,10 +31,65 @@ namespace Payroll.Models
         [Required]
         [MaxLength(16)]
         public string KK { set; get; }
+        [NotMapped]
+        public bool HasDriverLicense
+        {
+            get
+            {
+                if (DriverLicense == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public string DriverLicense { set; get; }
+        public DateTime DriverLicenseExpire { set; get; }
+        [DefaultValue("L")]
+        [Required]
         public string FamilyStatusCode { set; get; }
         public FamilyStatus FamilyStatus { set; get; }
+        [Required]
+        [MaxLength(14)]
+        public string BpjsNumber { set; get; }
+        public string BpjsRemark { set; get; }
+        [Required]
+        [MaxLength(12)]
+        public string JamsostekNumber { set; get; }
+        public string JamsostekRemark { set; get; }
+        public string NPWP { set; get; }
 
 
+        [Required]
+        public DateTime JoinCompanyDate { set; get; }
+        [Required]
+        public DateTime StartContract { set; get; }
+        [Required]
+        public DateTime EndContract { set; get; }
+
+        [Required]
+        public string BankCode { set; get; }
+        public Bank Bank { set; get; }
+        [Required]
+        public string AccountNumber { set; get; }
+        [Required]
+        public string AccountName { set; get; }
+        [Required]
+        public int EmploymentStatusId { set; get; }
+        public EmploymentStatus EmploymentStatus { set; get; }
+
+        [Column(TypeName = "bit")]
+        [DefaultValue(false)]
+        public bool HasUniform { set; get; }
+        [Column(TypeName = "bit")]
+        [DefaultValue(false)]
+        public bool HasIdCard { set; get; }
+        [Column(TypeName = "bit")]
+        [DefaultValue(false)]
+        public bool HasTraining { set; get; }
 
         [Required]
         public int PositionId { set; get; }
@@ -34,8 +100,9 @@ namespace Payroll.Models
         [Required]
         public int CustomerId { set; get; }
         public Customer Customer { set; get; }
+        [Required]
         public DateTime JoinCustomerDate { set; get; }
-        
+
 
     }
 }
