8479a5059b3c539842f2a3cfe852922a00c95c19
titoanugerah <titoanugerah@student.undip.ac.id>
2021-06-18 15:17:13 +0700

    Added position

---
+++ Models/Position.cs
@@ -0,0 +1,9 @@
+﻿namespace Payroll.Models
+{
+    public class Position : Audit
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Remark { set; get; }
+    }
+}
