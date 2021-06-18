f0914cd1a550896bade56a3f19ced1805812f2f2
titoanugerah <titoanugerah@student.undip.ac.id>
2021-06-18 15:26:44 +0700

    Added District

---
+++ Models/District.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Payroll.Models
+{
+    public class District : Audit
+    {
+        public int Id { set; get; }
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public int UMK { set; get; }
+        public string Remark { set; get; }
+    }
+}
