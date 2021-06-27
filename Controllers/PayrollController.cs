287342d0fc276cc08b3c95c8d26402ef15b604e5
titoanugerah <titoanugerah@student.undip.ac.id>
2021-06-28 04:27:40 +0700

    Added controller payroll

---
+++ Controllers/PayrollController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Payroll.Controllers
+{
+    public class PayrollController : Controller
+    {
+        private readonly ILogger<PayrollController> logger;
+        
+        public PayrollController(ILogger<PayrollController> _logger)
+        {
+            logger = _logger;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, "Payroll Controller - Index");
+                throw error;
+            }
+        }
+    }
+}
